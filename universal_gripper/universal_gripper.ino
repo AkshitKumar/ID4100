ed970121cc1200b3475642f9905a7b3c254e4f3f
Akshit Kumar <akshitkumar100@gmail.com>
2017-10-21 13:36:55 +0530

    add wifi setup code

--- universal_gripper/universal_gripper.ino
+++ universal_gripper/universal_gripper.ino
@@ -1,12 +1,42 @@
 #include <WiFi.h>
 #include <ros.h>
 
+const char* ssid = "UniversalGripper";
+const char* password = "";
+
+int status = WL_IDLE_STATUS;
+
 IPAddress server(192,168,1,100);
 const uint16_t serverPort = 11411;
 ros::NodeHandle nh;
 
+
+void printWiFiStatus(){
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+}
+
+void setupWiFi(){
+  while(status != WL_CONNECTED){
+    Serial.println("Attempting to connect to named network");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, password);
+    delay(1000);
+  }
+  printWiFiStatus();
+}
+
+
 void setup() {
+  Serial.begin(115200);
+  setupWiFi();
+  // TODO : Add the setupWifi code 
   nh.getHardware()->setConnection(server,serverPort);
+  nh.initNode();
 }
 
 void loop() {
