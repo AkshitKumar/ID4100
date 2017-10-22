e0f13c74ad9359486879387bc2276f605f1c44ff
Akshit Kumar <akshitkumar100@gmail.com>
2017-10-22 14:32:59 +0530

    Establish wireless connection between ESP and computer

--- universal_gripper/universal_gripper.ino
+++ universal_gripper/universal_gripper.ino
@@ -1,15 +1,19 @@
 #include <WiFi.h>
 #include <ros.h>
+#include <std_msgs/String.h>
 
 const char* ssid = "UniversalGripper";
 const char* password = "";
 
 int status = WL_IDLE_STATUS;
 
-IPAddress server(192,168,1,100);
+IPAddress server(192,168,0,100);
 const uint16_t serverPort = 11411;
 ros::NodeHandle nh;
+std_msgs::String str_msg;
+ros::Publisher chatter("chatter",&str_msg);
 
+char hello[13] = "hello world!";
 
 void printWiFiStatus(){
   Serial.print("SSID: ");
@@ -37,8 +41,12 @@ void setup() {
   // TODO : Add the setupWifi code 
   nh.getHardware()->setConnection(server,serverPort);
   nh.initNode();
+  nh.advertise(chatter);
 }
 
 void loop() {
-
+  str_msg.data = hello;
+  chatter.publish(&str_msg);
+  nh.spinOnce();
+  delay(1000);
 }
