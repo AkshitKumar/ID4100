e2523898e4f6f6d0053a547b21076040e8a533d3
Akshit Kumar <akshitkumar100@gmail.com>
2017-11-30 13:41:10 +0530

    change the gripper node to have new messages

--- gripper_control/src/control/scripts/gripper.py
+++ gripper_control/src/control/scripts/gripper.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import Joy
-from rover_msgs.msg import WheelVelocity
+#from rover_msgs.msg import WheelVelocity
+from rover_msgs.msg import gripper
 
+#gripper_value=WheelVelocity()
+gripper_value = gripper()
 
-gripper_value=WheelVelocity()
-
-pub=rospy.Publisher("gripper",WheelVelocity, queue_size=10)
+#pub=rospy.Publisher("gripper", WheelVelocity, queue_size=10)
+pub=rospy.Publisher("gripper", gripper , queue_size=10)
 
 
 def callback(msg):
-	gripper_value.left=40*msg.axes[1]
+	#gripper_value.left=40*msg.axes[1]
+	gripper_value.primary_act=40*msg.axes[1]
 	pub.publish(gripper_value)
 
 
 
 def main():
-	rospy.init_node("Gripper_node")
+	rospy.init_node("gripper_node")
 	rospy.Subscriber('/joy', Joy, callback)
 	rate=rospy.Rate(10)
 	while not rospy.is_shutdown():
